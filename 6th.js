e4208e0020aecb4b82f7efc22b4774a32d5226be
Karan-singh785 <karanbisht151003@gmail.com>
2024-07-03 21:18:24 +0530

    javascript

---
+++ 6th.js
@@ -0,0 +1,24 @@
+/*let height=1.63;
+let weight=60;
+let bmi=weight/(height*height);
+if(bmi<18.5)
+    {
+        console.log("underweight");
+    }
+else if (bmi<=24)
+    {
+        console.log("overweight");
+    }
+    else{
+        console.log("obesity");
+
+    }*/
+   let i;
+   for(i=1;i<=10;i++){
+
+    
+    if(i=5){
+        break;
+    }
+   }
+  
